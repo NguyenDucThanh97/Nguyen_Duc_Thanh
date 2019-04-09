73e96536bfc77ab8b0d4c3dc644c54e40b972002
NguyenDucThanh97 <47983762+NguyenDucThanh97@users.noreply.github.com>
2019-04-09 19:20:55 +0700

    unit 13

---
+++ Unit13/js/script.js
@@ -0,0 +1,8 @@
+new WOW().init();
+$(document).ready(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
+alert('Click Menu Tab để chuyển đến mục cần xem. Ex: Home, About ...');
